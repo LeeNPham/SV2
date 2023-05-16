78e1bd9418cde184caca74bb30267c89e5431820
Lee Pham <leenpham@gmail.com>
2023-05-15 22:45:46 -0700

    updated config for tailwind

--- Frontend/tailwind.config.cjs
+++ Frontend/tailwind.config.cjs
@@ -5,9 +5,36 @@ const config = {
 	],
 
 	theme: {
-		extend: {}
+		extend: {},
+		colors: {
+			blue: {
+				primary: '#0057AF',
+				dark: '#08429f',
+				verylight: '#ecfbff',
+				accessrequests: '#cdf6ff',
+				activity: '#6097b9'
+			},
+			gray: {
+				quickshare: '#f5f9fd',
+				groups: '#9baebc',
+				verylight: '#f3f7fd'
+			},
+			cyan: {
+				icon: '#00C3E7'
+			},
+			white: {
+				heading: '#f7fdff'
+			},
+			yellow: {
+				recentshares: '#ffebcc'
+			},
+			red: {
+				primary: '#FF0000',
+				dark: '#A10500',
+				activity: '#FF7777'
+			}
+		}
 	},
-
 	plugins: [require('flowbite/plugin')],
 	darkMode: 'class'
 };
