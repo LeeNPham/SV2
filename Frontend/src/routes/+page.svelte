01d564c93ae404b9262753834b5981d8319c29be
Lee Pham <leenpham@gmail.com>
2023-05-20 20:08:38 -0700

    quicksave

--- Frontend/src/routes/+page.svelte
+++ Frontend/src/routes/+page.svelte
@@ -44,6 +44,7 @@
 		'category-orange',
 		'category-purple'
 	];
+
 	let title = '';
 	let description = '';
 	let completion = false;
@@ -51,53 +52,56 @@
 	let completeCategories: any = [];
 
 	function buildCategoriesWithTodos(categories: any[], tasks: any[]) {
-		completeCategories['All'] = { todos: tasks }; // adding the All mapping of all tasks to the object
-		// Create map of categories with category name as key, and blank lists as their values
-		for (let category of categories) {
-			if (!completeCategories.hasOwnProperty(category.title)) {
-				let key = category.title;
-				completeCategories[key] = {};
-			}
-		}
 
-		// Iterate over tasks and adds task to correlating categorys list value
-		for (let task of tasks) {
-			let newKey = task.category;
-			if (completeCategories.hasOwnProperty(newKey)) {
-				if (!completeCategories[newKey].hasOwnProperty('todos')) {
-					completeCategories[newKey] = { todos: [task] };
-				} else {
-					completeCategories[newKey].todos.push(task);
-				}
-			}
-		}
+		console.log(categories)
+		console.log(categories[0]._id)  // start here later, you need the category id per category inside of your complete categories object so that you can delete it you dummy,,, and update it too I guess
+		// completeCategories['All'] = { todos: tasks }; // adding the All mapping of all tasks to the object
+		// // Create map of categories with category name as key, and blank lists as their values
+		// for (let category of categories) {
+		// 	if (!completeCategories.hasOwnProperty(category.title)) {
+		// 		let key = category.title;
+		// 		completeCategories[key] = {};
+		// 	}
+		// }
 
-		// Iterate over tasks and add a styling object to the categories list value to add styling ability
-		let i = 0;
-		for (let categoryObject in completeCategories) {
-			if (!completeCategories[categoryObject].hasOwnProperty('color')) {
-				completeCategories[categoryObject].color = colors[i];
-				i++;
-			}
-		}
+		// // Iterate over tasks and adds task to correlating categorys list value
+		// for (let task of tasks) {
+		// 	let newKey = task.category;
+		// 	if (completeCategories.hasOwnProperty(newKey)) {
+		// 		if (!completeCategories[newKey].hasOwnProperty('todos')) {
+		// 			completeCategories[newKey] = { todos: [task] };
+		// 		} else {
+		// 			completeCategories[newKey].todos.push(task);
+		// 		}
+		// 	}
+		// }
 
-		for (let categoryObject in completeCategories) {
-			if (!completeCategories[categoryObject].hasOwnProperty('count')) {
-				console.log(completeCategories[categoryObject]);
-				let count = completeCategories[categoryObject].todos.length;
-				completeCategories[categoryObject].count = count;
-			}
-		}
+		// // Iterate over tasks and add a styling object to the categories list value to add styling ability
+		// let i = 0;
+		// for (let categoryObject in completeCategories) {
+		// 	if (!completeCategories[categoryObject].hasOwnProperty('color')) {
+		// 		completeCategories[categoryObject].color = colors[i];
+		// 		i++;
+		// 	}
+		// }
 
-		// this is what I will use to do the rest of the rendering!
-		console.log(
-			'complete categories with color assignment and number of todos per category!',
-			completeCategories
-		);
+		// for (let categoryObject in completeCategories) {
+		// 	if (!completeCategories[categoryObject].hasOwnProperty('count')) {
+		// 		console.log(completeCategories[categoryObject]);
+		// 		let count = completeCategories[categoryObject].todos.length;
+		// 		completeCategories[categoryObject].count = count;
+		// 	}
+		// }
+
+		// // this is what I will use to do the rest of the rendering!
+		// console.log(
+		// 	'complete categories with color assignment and number of todos per category!',
+		// 	completeCategories
+		// );
 	}
 
 	function switchCategories(e: any) {
-		console.log(e.currentTarget.id); // selects the category, we should create a todosMap
+		//console.log(e.currentTarget.id); // selects the category, we should create a todosMap
 	}
 
 	function createTodo() {
@@ -161,20 +165,21 @@
 
 	async function deleteCategory(e: any) {
 		console.log('delete me');
-		console.log(e.target.parentElement.id);
-		category_id = e.target.parentElement.id;
-		const response = await fetch(`http://127.0.0.1:8000/api/category/${category_id}`, {
-			method: 'DELETE',
-			headers: {
-				'Content-Type': 'application/json'
-			}
-		})
-			.then((response) => {
-				window.location = '/';
-			})
-			.catch((error) => {
-				err = !err;
-			});
+		console.log(e)
+		// console.log(e.target.parentElement.id);
+		// category_id = e.target.parentElement.id;
+		// const response = await fetch(`http://127.0.0.1:8000/api/category/${category_id}`, {
+		// 	method: 'DELETE',
+		// 	headers: {
+		// 		'Content-Type': 'application/json'
+		// 	}
+		// })
+		// 	.then((response) => {
+		// 		window.location = '/';
+		// 	})
+		// 	.catch((error) => {
+		// 		err = !err;
+		// 	});
 	}
 
 	onMount(() => {
@@ -228,131 +233,62 @@
 			<div class="grid grid-cols-1 w-full">
 				<div class="flex overflow-scroll">
 					<div class="flex pb-10 px-3 gap-3">
-						<!-- Category -->
-						<button id="All" on:click={switchCategories} class="text-left" type="button">
-							<div
-								class="grid grid-cols-1 content-between min-w-[200px] bg-palette-dark h-[120px] rounded-3xl shadow-black/50 shadow-lg p-5"
-							>
-								<div class="grid grid-cols-1 gap-1">
-									<div class="text-palette-lightgray text-sm">{tasksCount} tasks</div>
-									<div class="text-white text-2xl font-bold">All</div>
-								</div>
-
-								<hr class="border-category-cyan shadow shadow-category-cyan" />
-							</div>
-						</button>
-						{#each Todos.categories as category, i}
-							<!-- <button on:click|self={switchCategories} class="relative text-left" type="button"> -->
+						{#each Object.keys(completeCategories) as category, i}
 							<button
-								id={category.title}
-								on:click={switchCategories}
-								class="relative text-left"
-								type="button"
-							>
-								<button
-									on:click={deleteCategory}
-									id={category._id}
-									class="absolute right-5 top-5"
-									type="button"
-								>
-									<svg
-										viewBox="0 0 576 512"
-										id={category._id}
-										role="img"
-										class="h-5 w-f fill-red-600 hover:fill-red-600/50"
-										><title>Delete</title><path
-											d="M576 384c0 35.3-28.7 64-64 64H205.3c-17 0-33.3-6.7-45.3-18.7L9.372 278.6C3.371 272.6 0 264.5 0 256c0-8.5 3.372-16.6 9.372-22.6L160 82.75C172 70.74 188.3 64 205.3 64H512c35.3 0 64 28.65 64 64v256zM271 208.1l47.1 47.9-47.1 47c-9.3 9.4-9.3 24.6 0 33.1 9.4 10.2 24.6 10.2 33.1 0l47.9-46.2 47 46.2c9.4 10.2 24.6 10.2 33.1 0 10.2-8.5 10.2-23.7 0-33.1l-46.2-47 46.2-47.9c10.2-8.5 10.2-23.7 0-33.1-8.5-9.3-23.7-9.3-33.1 0l-47 47.1-47.9-47.1c-8.5-9.3-23.7-9.3-33.1 0-9.3 9.4-9.3 24.6 0 33.1z"
-										/></svg
-									>
-								</button>
-								<div
-									class="grid grid-cols-1 content-between min-w-[200px] bg-palette-dark h-[120px] rounded-3xl shadow-black/50 shadow-lg p-5"
-								>
-									<div class="grid grid-cols-1 gap-1">
-										<div class="text-palette-lightgray text-sm">{tasksCount} tasks</div>
-										<div id={category._id} class="text-white text-2xl font-bold">
-											{category.title}
-										</div>
-									</div>
+											id={category}
+											on:click={switchCategories}
+											class="relative text-left"
+											type="button"
+										>
+										{#if category != 'All'}
+											<button
+												on:click={deleteCategory}
 
-									<hr class={categoryColors[i]} />
-								</div>
-							</button>
-						{/each}
+												class="absolute right-5 top-5"
+												type="button"
+											>
+												<svg
+													viewBox="0 0 576 512"
+
+													role="img"
+													class="h-5 w-f fill-red-600 hover:fill-red-600/50"
+													><title>Delete</title><path
+														d="M576 384c0 35.3-28.7 64-64 64H205.3c-17 0-33.3-6.7-45.3-18.7L9.372 278.6C3.371 272.6 0 264.5 0 256c0-8.5 3.372-16.6 9.372-22.6L160 82.75C172 70.74 188.3 64 205.3 64H512c35.3 0 64 28.65 64 64v256zM271 208.1l47.1 47.9-47.1 47c-9.3 9.4-9.3 24.6 0 33.1 9.4 10.2 24.6 10.2 33.1 0l47.9-46.2 47 46.2c9.4 10.2 24.6 10.2 33.1 0 10.2-8.5 10.2-23.7 0-33.1l-46.2-47 46.2-47.9c10.2-8.5 10.2-23.7 0-33.1-8.5-9.3-23.7-9.3-33.1 0l-47 47.1-47.9-47.1c-8.5-9.3-23.7-9.3-33.1 0-9.3 9.4-9.3 24.6 0 33.1z"
+													/></svg
+												>
+											</button>
+											{/if}
+											<div
+												class="grid grid-cols-1 content-between min-w-[200px] bg-palette-dark h-[120px] rounded-3xl shadow-black/50 shadow-lg p-5"
+											>
+												<div class="grid grid-cols-1 gap-1">
+													<div class="text-palette-lightgray text-sm">{completeCategories[category].count} tasks</div>
+													<div class="text-white text-2xl font-bold">
+														{category}
+													</div>
+												</div>
+
+												<hr class={completeCategories[category].color} />
+											</div>
+										</button>
+							{/each}
 						<button on:click={displayCreateNewCategoryModal} class="text-left" type="button">
-							<div
-								class="grid grid-cols-1 content-between min-w-[200px] bg-palette-dark h-[120px] rounded-3xl shadow-black/50 shadow-lg p-5"
-							>
-								<div class="grid grid-cols-1 gap-1">
-									<div class="text-palette-lightgray text-sm">tasks</div>
-									<div class="text-white text-2xl font-bold">+ Category</div>
-								</div>
+										<div
+											class="grid grid-cols-1 content-between min-w-[200px] bg-palette-dark h-[120px] rounded-3xl shadow-black/50 shadow-lg p-5"
+										>
+											<div class="grid grid-cols-1 gap-1">
+												<div class="text-palette-lightgray text-sm">tasks</div>
+												<div class="text-white text-2xl font-bold">+ Category</div>
+											</div>
 
-								<hr class="border-category-cyan shadow shadow-category-cyan" />
-							</div>
-						</button>
+											<hr class="border-category-cyan shadow shadow-category-cyan" />
+										</div>
+									</button>
 					</div>
 				</div>
 			</div>
 		</div>
 
-		<!-- Test Start -->
-		<!-- <div class="flex pb-10 px-3 gap-3">
-			{#each Object.keys(completeCategories) as category, i}
-				{#each completeCategories[category] as it}
-					<div class="text-lg font-bold text-white">{category}</div>
-					<pre>{console.log('this is my item', it)}</pre> -->
-		<!-- <button
-								id={category.title}
-								on:click={switchCategories}
-								class="relative text-left"
-								type="button"
-							>
-								<button
-									on:click={deleteCategory}
-									id={category._id}
-									class="absolute right-5 top-5"
-									type="button"
-								>
-									<svg
-										viewBox="0 0 576 512"
-										id={category._id}
-										role="img"
-										class="h-5 w-f fill-red-600 hover:fill-red-600/50"
-										><title>Delete</title><path
-											d="M576 384c0 35.3-28.7 64-64 64H205.3c-17 0-33.3-6.7-45.3-18.7L9.372 278.6C3.371 272.6 0 264.5 0 256c0-8.5 3.372-16.6 9.372-22.6L160 82.75C172 70.74 188.3 64 205.3 64H512c35.3 0 64 28.65 64 64v256zM271 208.1l47.1 47.9-47.1 47c-9.3 9.4-9.3 24.6 0 33.1 9.4 10.2 24.6 10.2 33.1 0l47.9-46.2 47 46.2c9.4 10.2 24.6 10.2 33.1 0 10.2-8.5 10.2-23.7 0-33.1l-46.2-47 46.2-47.9c10.2-8.5 10.2-23.7 0-33.1-8.5-9.3-23.7-9.3-33.1 0l-47 47.1-47.9-47.1c-8.5-9.3-23.7-9.3-33.1 0-9.3 9.4-9.3 24.6 0 33.1z"
-										/></svg
-									>
-								</button>
-								<div
-									class="grid grid-cols-1 content-between min-w-[200px] bg-palette-dark h-[120px] rounded-3xl shadow-black/50 shadow-lg p-5"
-								>
-									<div class="grid grid-cols-1 gap-1">
-										<div class="text-palette-lightgray text-sm">{tasksCount} tasks</div>
-										<div id={category._id} class="text-white text-2xl font-bold">
-											{category.title}
-										</div>
-									</div>
-
-									<hr class={categoryColors[i]} />
-								</div>
-							</button> -->
-		<!-- {/each}
-			{/each} -->
-		<!-- <button on:click={displayCreateNewCategoryModal} class="text-left" type="button">
-							<div
-								class="grid grid-cols-1 content-between min-w-[200px] bg-palette-dark h-[120px] rounded-3xl shadow-black/50 shadow-lg p-5"
-							>
-								<div class="grid grid-cols-1 gap-1">
-									<div class="text-palette-lightgray text-sm">tasks</div>
-									<div class="text-white text-2xl font-bold">+ Category</div>
-								</div>
-
-								<hr class="border-category-cyan shadow shadow-category-cyan" />
-							</div>
-						</button> -->
-		<!-- </div> -->
-		<!-- Test End -->
 
 		<!-- Today's Tasks -->
 		<div class="grid grid-cols-1 justify-start">
