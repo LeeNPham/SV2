c01aa14ac8a3a790b74dd397fc717d533c0d876f
Lee Pham <leenpham@gmail.com>
2023-06-24 16:48:03 -0700

    created access token utility function

--- Backend-Accounts/main.py
+++ Backend-Accounts/main.py
@@ -86,6 +86,18 @@ def authenticate_user(db, username: str, password: str):
         return False
     return user
 
+# this is actually just the JWT token
+def create_access_token(data:dict, expires_delta: timedelta or None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+
+    to_encode.update({'exp': expire})
+    encoded_jwt = jwt.encode(to_encode, secret_key, algorithm=algorithm)
+    return encoded_jwt
+
 app = FastAPI()
 
 
