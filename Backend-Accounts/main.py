9b90a45a13a8e0b9197cde851a43a6731c6ac99c
Lee Pham <leenpham@gmail.com>
2023-06-24 22:50:48 -0700

    add login for access token

--- Backend-Accounts/main.py
+++ Backend-Accounts/main.py
@@ -44,8 +44,10 @@ class Token(BaseModel):
     access_token: str
     token_type: str
 
+
 class TokenData(BaseModel):
     username: str or None = None
+
 
 class User(BaseModel):
     username: str
@@ -53,13 +55,18 @@ class User(BaseModel):
     full_name: str or None = None
     disabled: bool or None = None
 
+
 class UserInDB(User):
     hashed_password: str
 
+
 # This is going to define where we go to retrieve our access token
 # Allows us to verify password after hashing, and to has a password provided by the user
-pwd_context= CryptContext(schemes=['bcrypt'], deprecated='auto')
-oauth_2_scheme = OAuth2PasswordBearer(tokenUrl='token')
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth_2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+app = FastAPI()
+
 
 # Utility functions to authenticate our users and hash their passwords
 #   hashing means to perform encryption on the password so we aren't storing it as plain text
@@ -67,16 +74,19 @@ oauth_2_scheme = OAuth2PasswordBearer(tokenUrl='token')
 def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
 
+
 def get_password_hash(password):
     return pwd_context.hash(password)
+
 
 # This will initialize our model which comes from UserInDB, which inherits from User, which inherits from BaseModel
 #   by passing in all of the keyword arguments we got from our user_data dictionary
 #       (creates an object based on the keyword provided for all subkey/subvalue pairs)
-def get_user(db, username:str):
+def get_user(db, username: str):
     if username in db:
         user_data = db[username]
         return UserInDB(**user_data)
+
 
 def authenticate_user(db, username: str, password: str):
     user = get_user(db, username)
@@ -86,91 +96,68 @@ def authenticate_user(db, username: str, password: str):
         return False
     return user
 
+
 # this is actually just the JWT token
-def create_access_token(data:dict, expires_delta: timedelta or None = None):
+def create_access_token(data: dict, expires_delta: timedelta or None = None):
     to_encode = data.copy()
     if expires_delta:
         expire = datetime.utcnow() + expires_delta
     else:
         expire = datetime.utcnow() + timedelta(minutes=15)
 
-    to_encode.update({'exp': expire})
+    to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, secret_key, algorithm=algorithm)
     return encoded_jwt
 
 
 # Functions to getting our user from an access token, and if we're creating one based on login data
-    # calls out our oauth_2_scheme which will parse our our token and give us access to it in this parameter
-    # jwt.encode takes 1 algo
-    # jwt.decode takes multiple algos
+# calls out our oauth_2_scheme which will parse our our token and give us access to it in this parameter
+# jwt.encode takes 1 algo
+# jwt.decode takes multiple algos
 async def get_current_user(token: str = Depends(oauth_2_scheme)):
-    credential_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
-                                        detail='Could not validate credentials',
-                                        headers={'WWW-Authenticate': 'Bearer'})
+    credential_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
     try:
         payload = jwt.decode(token, secret_key, algorithms=[algorithm])
-        username: str = payload.get('sub')
+        username: str = payload.get("sub")
         if username is None:
             raise credential_exception
-
         token_data = TokenData(username=username)
     except JWTError:
         raise credential_exception
     user = get_user(db, username=token_data.username)
-
     if user is None:
         raise credential_exception
-
     return user
 
 
-app = FastAPI()
+# because it's possible the user may not actually be active,
+#   we could say disable a user and make then not able to login,
+#       if that's the case, we dont want to actually allow them to login
+async def get_current_active_user(current_user: UserInDB = Depends(get_current_user)):
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# Now to write a url that is /token
+@app.post("/token", response_model=Token)
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
+    user = authenticate_user(db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=access_token_expire_minutes)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
 
 
 # original reference
