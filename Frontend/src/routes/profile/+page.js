b012ca2dd697585d7a618b79d76c573952e327bf
Lee Pham <leenpham@gmail.com>
2023-07-02 00:09:39 -0700

    updated to cleaning up code

--- Frontend/src/routes/profile/+page.js
+++ Frontend/src/routes/profile/+page.js
@@ -4,9 +4,11 @@ export const prerender = true
 
 export function load() {
 	let userUsername = ''
+
 	user_username.subscribe((value) => {
 		userUsername = value.toLowerCase()
 	})
+
 	async function getUserDetails() {
 		const response = await fetch(
 			`https://accounts-79lp.onrender.com/api/user/username/${userUsername}`
@@ -14,7 +16,9 @@ export function load() {
 		const data = await response.json()
 		return data
 	}
+
 	const user = getUserDetails()
+
 	return {
 		user: user
 	}
