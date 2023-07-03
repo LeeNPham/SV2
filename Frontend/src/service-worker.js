66aaeb4c6a9034c2bfeda5f05a46d0534672802c
Lee Pham <leenpham@gmail.com>
2023-07-02 17:53:33 -0700

    removed native downloading ability

--- Frontend/src/service-worker.js
+++
@@ -1,9 +0,0 @@
-import { build, files, prerendered, version } from '$service-worker'
-import { precacheAndRoute } from 'workbox-precaching'
-
-const precache_list = [...build, ...files, ...prerendered].map((file) => ({
-	url: file,
-	revision: version
-}))
-
-precacheAndRoute(precache_list)
