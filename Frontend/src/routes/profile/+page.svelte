95ddb851f4a883df4ef935e2fd9f94c35fe9ef44
Lee Pham <leenpham@gmail.com>
2023-07-06 00:33:18 -0700

    aye most of the stuff is working again!

--- Frontend/src/routes/profile/+page.svelte
+++ Frontend/src/routes/profile/+page.svelte
@@ -34,8 +34,8 @@
 				method: 'GET',
 				headers: {
 					'Content-Type': 'image/jpg'
-				},
-				mode: 'no-cors'
+				}
+				// mode: 'no-cors'
 			})
 			if (!response.ok) {
 				throw new Error('Failed to fetch user profile image')
