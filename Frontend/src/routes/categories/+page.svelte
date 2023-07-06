0c73379137eeffca07cc25471e776fcfcfb25a6c
Lee Pham <leenpham@gmail.com>
2023-07-06 01:35:19 -0700

    added temp cats

--- Frontend/src/routes/categories/+page.svelte
+++ Frontend/src/routes/categories/+page.svelte
@@ -29,14 +29,21 @@
 		return newCategories
 	}
 
+	let list1 = [
+		1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26,
+		27, 28, 29, 30
+	]
+
 	onMount(async () => {
 		myCategories = filterToMyCategories(data.identity.categories, data.categories)
 		console.log(myCategories)
 	})
 </script>
 
-<div class="min-w-[414px] flex flex-col justify-center h-screen px-8 py-20 bg-slate-400">
-	<div class="flex flex-row justify-between bg-gray-700 h-1/5">
+<div
+	class="min-w-[414px] flex flex-col justify-center h-screen px-8 py-10 bg-slate-400 rounded-3xl"
+>
+	<div class="flex flex-row items-start justify-between bg-gray-700 h-1/10">
 		<button class="flex items-center" type="button" on:click={goProfile}>
 			<div class="text-white">Profile</div>
 			<ChevronWithLeftCircle Class="fill-palette-gray/80 h-14 w-14" />
@@ -47,9 +54,17 @@
 			<ChevronWithLeftCircle Class="fill-palette-gray/80 h-14 w-14" />
 		</button>
 	</div>
-	<div class="grid grid-cols-1 w-full px-4 gap-5 bg-blue-400 h-4/5 content-start py-10">
-		{#each myCategories as category}
+	<div class="flex justify-center text-white font-bold text-3xl">My Categories</div>
+	<button class="text-white bg-palette-dark rounded-lg my-2 py-2">+ Category</button>
+	<div
+		class="grid grid-cols-2 w-full gap-5 bg-palette-lightgray h-4/5 content-start py-10 overflow-y-scroll"
+	>
+		<!-- {#each myCategories as category}
 			<div class="w-full bg-red-600 border-2 rounded-xl h-20">{category.title}</div>
+		{/each} -->
+
+		{#each list1 as category}
+			<div class="w-full bg-palette-dark text-white rounded-2xl h-32 shadow-inner">{category}</div>
 		{/each}
 	</div>
 </div>
