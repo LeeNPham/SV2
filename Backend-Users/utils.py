87624bae4309fbde6666a8166131a9f27fdae732
Lee Pham <leenpham@gmail.com>
2023-07-08 02:04:29 -0700

    updates to routing

--- Backend-Users/utils.py
+++ Backend-Users/utils.py
@@ -65,7 +65,6 @@ async def get_current_user(token: str = Depends(oauth_2_scheme)):
         raise credential_exception
     user = await fetch_one_user_by_username(username=token_data.username)
     if user is None:
-        print("error3")
         raise credential_exception
     return user
 
--- Frontend/src/lib/components/NavMenu.svelte
+++ Frontend/src/lib/components/NavMenu.svelte
@@ -11,6 +11,9 @@
 	import MenuIcon from '$lib/icons/MenuIcon.svelte'
 	import Categories from '$lib/icons/Categories.svelte'
 	import { categoriesCountStore } from '$store/stores'
+	import { goto } from '$app/navigation'
+	import HouseIcon from '$lib/icons/HouseIcon.svelte'
+	import ProfileIcon from '$lib/icons/ProfileIcon.svelte'
 
 	let hidden2 = true
 
@@ -21,18 +24,18 @@
 	}
 
 	function goProfile() {
-		window.location.assign('/profile')
+		goto('/profile')
 	}
 
 	function goHome() {
-		window.location.assign('/home')
+		goto('/home')
 	}
 
 	function goCategories() {
-		window.location.assign('/categories')
+		goto('/categories')
 	}
 	const logout = () => {
-		window.location.assign('/')
+		goto('/')
 	}
 </script>
 
@@ -60,75 +63,54 @@
 	<Sidebar>
 		<SidebarWrapper divClass="overflow-y-auto py-4 px-3 rounded dark:bg-gray-800">
 			<SidebarGroup ulClass="flex flex-col">
-				<button on:click={goHome}>
-					<SidebarItem label="Home">
-						<svelte:fragment slot="icon">
-							<svg
-								viewBox="0 0 24 24"
-								focusable="false"
-								role="img"
-								xmlns="http://www.w3.org/2000/svg"
-								class="fill-palette-dark hover:fill-palette-gray h-7 w-7"
-								><path
-									d="m12.223 11.641-.223.22-.224-.22a2.224 2.224 0 0 0-3.125 0 2.13 2.13 0 0 0 0 3.07L12 18l3.349-3.289a2.13 2.13 0 0 0 0-3.07 2.225 2.225 0 0 0-3.126 0z"
-								/><path
-									d="m21.707 11.293-9-9a.999.999 0 0 0-1.414 0l-9 9A1 1 0 0 0 3 13h1v7c0 1.103.897 2 2 2h12c1.103 0 2-.897 2-2v-7h1a1 1 0 0 0 .707-1.707zM18.001 20H6v-9.585l6-6 6 6V15l.001 5z"
-								/></svg
-							>
-						</svelte:fragment>
-					</SidebarItem>
-				</button>
-				<button on:click={goProfile}>
-					<SidebarItem label="Profile">
-						<svelte:fragment slot="icon">
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								fill="none"
-								viewBox="0 0 24 24"
-								stroke-width="1.5"
-								stroke="currentColor"
-								class="w-6 h-6"
-								><path
-									stroke-linecap="round"
-									stroke-linejoin="round"
-									d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
-								/></svg
-							>
-						</svelte:fragment>
-					</SidebarItem>
-				</button>
-				<button on:click={goCategories}>
-					<SidebarItem label="Categories">
-						<svelte:fragment slot="icon">
-							<Categories Class="fill-palette-dark hover:fill-palette-gray h-6 w-6" />
-						</svelte:fragment>
-						<svelte:fragment slot="subtext">
-							<span
-								class="inline-flex justify-center items-center p-3 ml-3 w-3 h-3 text-sm font-medium text-primary-600 bg-primary-200 rounded-full dark:bg-primary-900 dark:text-primary-200"
-								>{$categoriesCountStore}</span
-							>
-						</svelte:fragment>
-					</SidebarItem>
-				</button>
-				<button on:click={logout}>
-					<SidebarItem label="Log out">
-						<svelte:fragment slot="icon">
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								fill="none"
-								viewBox="0 0 24 24"
-								stroke-width="1.5"
-								stroke="currentColor"
-								class="w-6 h-6"
-								><path
-									stroke-linecap="round"
-									stroke-linejoin="round"
-									d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75"
-								/></svg
-							>
-						</svelte:fragment>
-					</SidebarItem>
-				</button>
+				<a
+					href="/home"
+					on:click={() => (hidden2 = true)}
+					class="flex flex-row items-center gap-4 hover:bg-palette-medium rounded-xl px-2 py-1"
+				>
+					<HouseIcon />
+					<div class="font-semibold">Home</div>
+				</a>
+
+				<a
+					href="/profile"
+					on:click={() => (hidden2 = true)}
+					class="flex flex-row items-center gap-4 hover:bg-palette-medium rounded-xl px-2 py-1"
+				>
+					<ProfileIcon />
+					<div class="font-semibold">Profile</div>
+				</a>
+
+				<a
+					href="/categories"
+					on:click={() => (hidden2 = true)}
+					class="flex flex-row items-center gap-4 hover:bg-palette-medium rounded-xl px-2 py-1"
+				>
+					<Categories Class="fill-palette-dark hover:fill-palette-gray h-[25px] w-[25px] ml-0.5" />
+					<div class="font-semibold">Categories</div>
+					<span
+						class="inline-flex justify-center items-center p-3 ml-3 w-3 h-3 text-sm font-medium text-primary-600 bg-primary-200 rounded-full dark:bg-primary-900 dark:text-primary-200"
+						>{$categoriesCountStore}</span
+					>
+				</a>
+
+				<a
+					href="/"
+					on:click={() => (hidden2 = true)}
+					class="flex flex-row items-center gap-4 hover:bg-palette-medium rounded-xl px-2 py-1"
+				>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 24 24"
+						class="w-[25px] [25px] ml-0.5 stroke-2 stroke-palette-dark fill-none"
+						><path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75"
+						/></svg
+					>
+					<div class="font-semibold">Logout</div>
+				</a>
 			</SidebarGroup>
 		</SidebarWrapper>
 	</Sidebar>
